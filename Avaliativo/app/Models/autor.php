6000b16a1375c38fa2c263e2c16febf953c2ca89
Taiane091 <127451386+Taiane091@users.noreply.github.com>
2024-05-23 00:08:17 +0000

    Added attributes to models

--- Avaliativo/app/Models/autor.php
+++ Avaliativo/app/Models/autor.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class autor extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nome_autor',
+    ];
 }
--- Avaliativo/app/Models/livro.php
+++ Avaliativo/app/Models/livro.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class livro extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nome_livro',
+        'isbn_livro',
+        'genero_livro',
+        'data_publ_livro',
+        'capa_livro',
+        'nr_pag_livro',
+        'idade_rec_livro',
+    ];
 }
